6f98436af1432fb30877331ecf468e4042b12f2b
RafaelFreitas <raf.mec.ba@gmail.com>
2023-03-08 18:19:34 -0300

    add input brocked yet

--- projeto-final/src/pages/RegisterPage/index.tsx
+++ projeto-final/src/pages/RegisterPage/index.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+
+
+import { SubmitHandler, useForm } from "react-hook-form";
+import Input from "../../components/InputDefault";
+import { IFormRegister } from "../../providers/UserContext/@types";
 
 export default function RegisterPage() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormRegister>();
+
+
+  const submit: SubmitHandler<IFormRegister> =(formData:IFormRegister)=>{
+    register(formData)
+  }
+  
+  return (
+    <form onSubmit={handleSubmit(submit)}>
+        <Input label="Seu nome" register={register("name")} error={errors.name}/>
+        <Input label="Seu email" register={register("email")} error={errors.email}/>
+        <Input label="Seu senha" register={register("password")} error={errors.password}/>
+        <button type="submit">Cadastrar</button>
+    </form>
+  )
 }
