af1c7a6025aa2d32ea267f7287d49ea8e1b432c4
Gael <rchsilver@gmail.com>
2023-03-13 10:50:15 -0300

    feat/styled home page

--- projeto-final/src/pages/HomePage/index.tsx
+++ projeto-final/src/pages/HomePage/index.tsx
@@ -1,9 +1,9 @@
 import React, { useContext } from "react";
+import { StyledHomePage } from "./style";
 import { SubmitHandler, useForm } from "react-hook-form";
 import Input from "../../components/InputDefault";
 import { UserContext } from "../../providers/UserContext";
 import { IFormLogin } from "../../providers/UserContext/@types";
-import { styledHomePage } from "./style";
 
 export default function HomePage() {
   const { goToRegister, userLogin } = useContext(UserContext);
@@ -19,23 +19,31 @@ export default function HomePage() {
   };
 
   return (
-    <styledHomePage>
+    <StyledHomePage>
       <form onSubmit={handleSubmit(submit)}>
+        <h1>Login</h1>
         <Input
+          className="input"
           label="Seu email"
           type="email"
           register={register("email")}
           error={errors.email}
         />
         <Input
+          className="input"
           label="Sua senha"
           type="password"
           register={register("password")}
           error={errors.password}
         />
-        <button type="submit">Login</button>
+        <button className="loginBtn" type="submit">
+          Entrar
+        </button>
+        <p>Ainda não possui conta?</p>
+        <button className="registerBtn" onClick={() => goToRegister()}>
+          Cadastre-se
+        </button>
       </form>
-      <button onClick={() => goToRegister()}>Cadastrar</button>
-    </styledHomePage>
+    </StyledHomePage>
   );
 }
